d886c2a979826dd5db9812b2feb4dc871bdf1d31
MikeCanto <mj_cantom@protonmail.com>
2023-09-21 14:09:49 -0600

    1.3 (Django Admin)

--- myapp/models.py
+++ myapp/models.py
@@ -5,8 +5,15 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
+    # ESTO LO QUE HACE ES QUE REGRESE LOS NOMBRES DE LOS OBJETOS EN LA TABLA
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.project.name
